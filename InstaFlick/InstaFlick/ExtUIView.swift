12115741a3bbb75ac4791c9d81062d8b0aeb8626
vfxdrummer <timothy.brandt@gmail.com>
2017-03-12 00:12:50 -0800

    Improve dot animation

--- InstaFlick/InstaFlick/ExtUIView.swift
+++ InstaFlick/InstaFlick/ExtUIView.swift
@@ -15,23 +15,66 @@ extension UIView {
   // Animation
   //
   
-  func addAnimationWithDelay(shapeLayer:CAShapeLayer, keyPath:String, fromValue:Double, toValue:Double, duration:Double, delay:Double, timingFunction:String) {
+  func getCABasicAnimation(keyPath:String, fromValue:Double, toValue:Double, beginTime:Double, duration:Double, timingFunction:String) -> CABasicAnimation {
     let pathAnimation: CABasicAnimation = CABasicAnimation(keyPath: keyPath)
     pathAnimation.timingFunction = CAMediaTimingFunction(name: timingFunction)
-    DispatchQueue.main.asyncAfter(deadline: (.now() + delay)) {
-      pathAnimation.duration = CFTimeInterval(duration)
-      pathAnimation.fromValue = NSNumber(value: fromValue)
-      pathAnimation.toValue = NSNumber(value: toValue)
-      shapeLayer.add(pathAnimation, forKey: keyPath)
-    }
+    pathAnimation.beginTime = beginTime
+    pathAnimation.duration = CFTimeInterval(duration)
+    pathAnimation.fromValue = NSNumber(value: fromValue)
+    pathAnimation.toValue = NSNumber(value: toValue)
+    pathAnimation.isCumulative = true
+    
+    return pathAnimation
   }
   
-  func animatedDot(withDistance delta: CGFloat, delay: Double) -> CALayer {
+  func addAnimationWithDelay(shapeLayer:CAShapeLayer, keyPath:String, fromValue:Double, toValue:Double, duration:Double, delay:Double, timingFunction:String) {
+    self.addChainedAnimations(delay:delay, shapeLayer:shapeLayer, keyPath:[keyPath], fromValue:[fromValue], toValue:[toValue], duration:[duration], timingFunction:[timingFunction])
+  }
+  
+  func addChainedAnimations(delay:Double, shapeLayer:CAShapeLayer, keyPath:[String], fromValue:[Double], toValue:[Double], duration:[Double],timingFunction:[String]) {
+    let min = [keyPath.count, fromValue.count, toValue.count, duration.count, timingFunction.count].min()
+    let animationGroup: CAAnimationGroup = CAAnimationGroup()
+    var beginTime = delay
+    var animations:[CABasicAnimation] = []
+    for i in 0..<min! {
+      let pathAnimation = self.getCABasicAnimation(keyPath:keyPath[i], fromValue:fromValue[i], toValue:toValue[i], beginTime:beginTime, duration:duration[i], timingFunction:timingFunction[i])
+      animations.append(pathAnimation)
+      beginTime += duration[i]
+    }
+    animationGroup.animations = animations
+    animationGroup.duration = beginTime
+    animationGroup.fillMode = kCAFillModeForwards
+    animationGroup.isRemovedOnCompletion = false
+    shapeLayer.add(animationGroup, forKey: nil)
+  }
+  
+  func animatedDot(withDistance delta: CGFloat, delay: Double, rate:Double) -> CALayer {
     let dot = CAShapeLayer()
-    dot.backgroundColor = UIColor.cyan.cgColor
-    dot.frame = CGRect(x: 0, y: -10, width: 1, height: 1)
+    dot.strokeColor = UIColor.cyan.cgColor
+    let circlePath = UIBezierPath(arcCenter: CGPoint(x: 0,y: -10), radius: 0.2, startAngle: CGFloat(0), endAngle:CGFloat(M_PI * 2), clockwise: true)
+    dot.path = circlePath.cgPath
     
-    self.addAnimationWithDelay(shapeLayer: dot, keyPath: "transform.translation.y", fromValue: 0.0, toValue: -(Double)(delta), duration: 1.0, delay: 0.0, timingFunction:kCAMediaTimingFunctionEaseOut)
+    let keyPaths:[String] = ["transform.translation.y", "transform.translation.y", "transform.translation.y"]
+    let durations = [rate*1.0, rate*2.0, rate*1.0]
+    let fromValues = [0.0, -(Double)(delta), -(Double)(delta)]
+    let toValues = [-(Double)(delta), -(Double)(delta), -2*(Double)(delta)]
+    let timingFunctions = [kCAMediaTimingFunctionEaseOut, kCAMediaTimingFunctionEaseOut, kCAMediaTimingFunctionEaseOut]
+    
+    self.addChainedAnimations(delay:delay, shapeLayer: dot, keyPath: keyPaths, fromValue: fromValues, toValue: toValues, duration: durations, timingFunction:timingFunctions)
+    
+    let keyPaths2:[String] = ["transform.rotation", "transform.rotation", "transform.rotation"]
+    let fromValues2 = [0.0, 0.0, M_PI_4]
+    let toValues2 = [0.0, M_PI_4, 0]
+    
+    self.addChainedAnimations(delay:delay, shapeLayer: dot, keyPath: keyPaths2, fromValue: fromValues2, toValue: toValues2, duration: durations, timingFunction:timingFunctions)
+    
+    self.addChainedAnimations(delay:delay, shapeLayer: dot, keyPath: keyPaths, fromValue: fromValues, toValue: toValues, duration: durations, timingFunction:timingFunctions)
+    
+    let keyPaths3:[String] = ["opacity", "opacity", "opacity"]
+    let fromValues3 = [1.0, 1.0, 1.0]
+    let toValues3 = [1.0, 1.0, 0]
+    
+    self.addChainedAnimations(delay:delay, shapeLayer: dot, keyPath: keyPaths3, fromValue: fromValues3, toValue: toValues3, duration: durations, timingFunction:timingFunctions)
     
     return dot
   }
@@ -99,10 +142,6 @@ extension UIView {
     self.addAnimationWithDelay(shapeLayer: f11line, keyPath: "transform.rotation", fromValue: 0.0, toValue: M_PI_4, duration: 2.0, delay: 0.0, timingFunction:kCAMediaTimingFunctionEaseOut)
     self.addAnimationWithDelay(shapeLayer: f12line, keyPath: "transform.rotation", fromValue: 0.0, toValue: M_PI_4/2, duration: 2.0, delay: 0.0, timingFunction:kCAMediaTimingFunctionEaseOut)
     
-    replicator.addSublayer(
-      animatedDot(withDistance: 0, delay: 0)
-    )
-    
     return replicator
   }
   
@@ -113,9 +152,11 @@ extension UIView {
     replicator.instanceCount = 40
     replicator.instanceTransform = CATransform3DMakeRotation(CGFloat(M_PI/20), 0, 0, 1)
     
-    replicator.addSublayer(
-      animatedDot(withDistance: 100, delay: 0)
-    )
+    for i in 1...10 {
+      replicator.addSublayer(
+        animatedDot(withDistance: CGFloat(i*10), delay: 1/Double(i), rate:0.5)
+      )
+    }
     
     return replicator
   }
